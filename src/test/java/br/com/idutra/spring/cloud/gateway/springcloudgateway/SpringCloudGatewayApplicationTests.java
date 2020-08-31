631ebee4b5bf9604e50d5b2bbe9eea932bb0ba76
igor.dutra <igor.dutra@totvs.com.br>
2020-08-31 13:50:31 -0300

    commit inicial do projeto

---
+++ src/test/java/br/com/idutra/spring/cloud/gateway/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.idutra.spring.cloud.gateway.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
