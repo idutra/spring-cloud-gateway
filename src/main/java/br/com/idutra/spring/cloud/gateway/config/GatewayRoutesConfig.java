0f94ee9516004289387796ead1387e0f0df220a8
igor.dutra <igor.dutra@totvs.com.br>
2020-08-31 14:18:20 -0300

    - Ajustando o pom.xml - Adicionando informações no README.md - Incluindo uma nova rota no GatewayRoutesConfig.minhasRotas

--- src/main/java/br/com/idutra/spring/cloud/gateway/config/GatewayRoutesConfig.java
+++ src/main/java/br/com/idutra/spring/cloud/gateway/config/GatewayRoutesConfig.java
@@ -1,15 +1,23 @@
 package br.com.idutra.spring.cloud.gateway.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
+@EnableAutoConfiguration
+@Primary
 public class GatewayRoutesConfig {
 
     @Bean
     public RouteLocator minhasRotas(RouteLocatorBuilder builder) {
-        return builder.routes().build();
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
     }
 }
